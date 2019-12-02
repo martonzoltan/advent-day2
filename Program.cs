b3d9582da4fdb4e940fa7830e4cfe6f3bf134e1a
Zoltan Marton <martonzoltan4@gmail.com>
2019-12-02 06:56:49 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting fuel calculation module");
+            //FirstPart();
+            SecondPart();
+        }
+        private static void FirstPart()
+        {
+            var modulesInNeetOfCalibrating = GetInput();
+        }
+
+        private static void SecondPart()
+        {
+            
+        }
+
+        private static string[] GetInput(){
+            var input = System.IO.File.ReadAllLines(@"input.txt");
+            return input;
+        }
+    }
+}
